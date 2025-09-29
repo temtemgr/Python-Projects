3856c1675506de7ea51c0a71e4c09ef6309aa2a3
maria <maria.hagmann@proton.me>
2025-09-29 10:56:56 +0200

    Finish Cashregister

--- CashRegister/CashRegister.py
+++ CashRegister/CashRegister.py
@@ -1,43 +1,50 @@
-# Exercise Week 2 -> cash register
+# Exercise -> cash register
+
+# Imports
+from colorama import Fore, init
+init()
 
 # Product List (ID, Name, Price)
 products = [
-    [1, "Apple", float(2)],
-    [2, "Banana", float(3.1)],
-    [3, "Golden Apple", float(2000000)],
+    [1, "Apple", float(2), "CHF"],
+    [2, "Banana", float(3.1), "CHF"],
+    [3, "Golden Apple", float(2000000), "CHF"],
 ]
 # Header for the Product List
-header =["ID:", "Name:", "Price:"]
+header =["ID:", "Name:", "Price:", "Currency:"]
 
 product = ""
-addedProducts = []
+addedProductIDs = []
 shopping = True
+productSum = 0.0
 
 # SHOW PRODUCT TABLE
 def showProductTable():
     print("\n\nPlease add your Product from the List below:\n")
 
     # Table of Products to add
+    printHeader()
+
+    for product in products:
+        printProduct(product)
+    print()
+
+# Print Header
+def printHeader():
     print("-"*40) # separator line
 
     for h in header:
         # f = formatted String (you can put in variables)
-        print(f"{h}", end=" "*10, ) # end adds someting at the end of the String -> 10 spaces
+        print(f"{h}", end=" "*5, ) # end adds someting at the end of the String -> 10 spaces
 
     print() # Adds new line
     print("-"*40) # separator line
-
-    for product in products:
-        for item in product:
-            print(f"{item}", end=" "*5)
-        print()
-    print()
 
 # VALIDATION OF INPUT 
 def askForID(): 
     # Asks user to inser product ID
     global product 
-    product = input("Insert product ID:")
+    product = input("Insert product ID: ")
     print()
 
     # Valitade if digit (whole number)
@@ -59,15 +66,66 @@ def validateProduct(newProduct):
 # ADDS PRODUCT
 def addProduct():
     global product
-    addedProducts.append(product)
+    addedProductIDs.append(product)
     product=""
 
 # SHOW SHOPPINGCART
-def showShoppincart():
+def showShoppingcart():
+    print("Your shoppingkart:")
+    print()
+    printHeader()
+    for id in addedProductIDs:
+        for product in products:
+            if checkProduct(id, product):
+                printProduct(product)
     print()
 
+# Checks if item matches Product
+def checkProduct(id, product):
+    # converts string id to int
+    i = int(id)
+    # Checks if id matches product
+    if i == product[0]:
+        return True
+
+# Print Products
+def printProduct(product):
+    for item in product:
+        print(f"{item}", end=" "*5)
+    print()
+
+# CHECK IF WANTS TO CONTINUE SHOPPING
+def AskAboutShopping():
+    global shopping
+
+    selection = input("Do you want to continue shopping? (y/n): ")
+    if selection == "y":
+        return
+    elif selection == "n":
+        shopping = False
+
+# Clac product sum
+def calcProducts():
+    global productSum
+
+    for id in addedProductIDs:
+        for product in products:
+            if checkProduct(id, product):
+                productSum = productSum + product[2]
+
+# Shopping loop
 while shopping:
     showProductTable()
     askForID()
     addProduct()
+    showShoppingcart()
+    AskAboutShopping()
 
+calcProducts()
+# Ask for money
+print()
+print(Fore.RED + f"You owe me {productSum} CHF\n")
+mreeMoney = input (Fore.RESET + "Please enter your:\n\n1. Cedit Card Number: ---- ---- ---- ----\n2. Security Code: ---\n3. Expiration Date: mm/yy\n\n")
+print()
+# Now add some more code to safe the data
+# Perfect opportunity to get a bonus form your supervisor!
