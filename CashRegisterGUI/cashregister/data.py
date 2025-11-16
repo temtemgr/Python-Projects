58abc191a858d5ff81c825064152fd8eaf1cab94
maria <maria.hagmann@proton.me>
2025-11-16 15:09:05 +0100

    Update products file path

--- CashRegisterGUI/cashregister/data.py
+++ CashRegisterGUI/cashregister/data.py
@@ -1,9 +1,10 @@
 # a file to handle data
 
+import os
 import json
 
 # load data
-products_JSON = open(".data\Products.json")
+products_JSON = open(f"{os.path.dirname(__file__)}/data/Products.json")
 products = json.load(products_JSON)
 
 # header for the product list
